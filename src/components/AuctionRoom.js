3007d2e6ea79d8bf73988d0795e28dafe9e719d9
Sam Naser <sammnaser@gmail.com>
2017-11-05 23:46:06 -0600

    Access selected auction and pass data down to auction room

--- src/components/AuctionRoom.js
+++ src/components/AuctionRoom.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { MediaBox } from 'react-materialize';
+
+//grab that thicc data
+import AUCTION_DATA from '../services/Api.js';
 
 //custom styles
 import '../styles/AuctionRoom.css';
@@ -7,11 +11,25 @@ class AuctionRoom extends Component {
 
   constructor(props){
       super(props);
+      this.state = {
+          auctionItem: AUCTION_DATA.AUCTION_DATA.find((auction) => {
+              console.log(this.props.match.params.auctionId);
+              console.log(auction.auctionId);
+              return this.props.match.params.auctionId == auction.auctionId;
+          })
+      }
+      console.log(this.state);
   }
 
   render() {
     return (
-        <div className="id-text">{this.props.match.params.auctionId}</div>
+        <div>
+            <MediaBox 
+                className="image-media"
+                src={this.state.auctionItem.imageUrl} 
+                caption={this.state.auctionItem.title} 
+                width="550"/>
+        </div>
     )
   }
 
