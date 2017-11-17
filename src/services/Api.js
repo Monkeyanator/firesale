459e0dd665baa928aeed694b1d3a57a938e9aeae
Sam Naser <sammnaser@gmail.com>
2017-11-17 15:43:06 -0600

    Style changes to card titles

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,42 +3,42 @@ module.exports = {
         
     AUCTION_DATA: [
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/1",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/1",
             title: "The Birth of Venus",
             bidders: 7,
             link: "https://monkeyanator.github.io/",
             auctionId: 1
         },
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/2",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/2",
             title: "The Last Supper",
             bidders: 2,
             link: "https://monkeyanator.github.io/",
             auctionId: 2
         },
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/3",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/3",
             title: "Original 1549 Mona Lisa, Pride of Venice, from Leonardo da Vinci",
             bidders: 8,
             link: "https://monkeyanator.github.io/",
             auctionId: 3
         },
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/4",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/4",
             title: "The Creation of Adam",
             bidders: 11,
             link: "https://monkeyanator.github.io/",
             auctionId: 4
         },
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/5",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/5",
             title: "Guernica",
             bidders: 11,
             link: "https://monkeyanator.github.io/",
             auctionId: 5
         },
         {
-            imageUrl: "http://lorempixel.com/400/260/nightlife/6",
+            imageUrl: "http://lorempixel.com/400/300/nightlife/6",
             title: "The Night Watch",
             bidders: 11,
             link: "https://monkeyanator.github.io/",
